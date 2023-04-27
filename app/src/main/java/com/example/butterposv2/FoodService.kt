4cf94fe89c21d54ed1a46c94cdd7082c5fabd647
vinny2001 <30963796+vinny2001@users.noreply.github.com>
2023-04-27 19:02:27 -0400

    Initial commit

---
+++ app/src/main/java/com/example/butterposv2/FoodService.kt
@@ -0,0 +1,16 @@
+package com.example.butterposv2
+
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface FoodService {
+    //decide here what api to access and write necessary signature
+
+    //below is restfull API for search by different options
+    //https://www.themealdb.com/api/json/v1/1/filter.php?a=American
+
+    @GET(".")
+    fun getFoodInfo(@Query("cuisine")Cuisine:String): Call<Food>
+}
