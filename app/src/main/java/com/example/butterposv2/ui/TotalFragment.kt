aa46e39a0bb1ccf807d72acd7eb06e05b3da7781
dpancakes117 <deqwonbentley@gmail.com>
2023-05-04 19:38:55 -0400

    These are the fragment kotlin files for the total support and a receipt. Still working on integration

---
+++ app/src/main/java/com/example/butterposv2/ui/TotalFragment.kt
@@ -0,0 +1,20 @@
+package com.example.butterposv2.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.butterposv2.R
+
+class TotalFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val root = inflater.inflate(R.layout.fragment_total, container, false)
+        // Do any additional setup for the child fragment here
+        return root
+    }
+}
