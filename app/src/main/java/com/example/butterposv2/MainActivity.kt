c77ea19645eaf952460bf839f96f15f853fcab78
vinny2001 <30963796+vinny2001@users.noreply.github.com>
2023-05-07 15:53:02 -0400

    First attempt at notifications, not working currently

--- app/src/main/java/com/example/butterposv2/MainActivity.kt
+++ app/src/main/java/com/example/butterposv2/MainActivity.kt
@@ -1,8 +1,17 @@
 package com.example.butterposv2
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.navigation.findNavController
@@ -20,7 +29,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -46,5 +54,41 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
     }
+
+    /*private fun createNotificationChannel(){
+        //If Android Oreo or above, use REQUIRED notification channel
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O ){
+            val name = "Order Status"
+            val descriptionText = "Your order has been placed!"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply{
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+    // For Versions < Oreo:
+    @SuppressLint("MissingPermission")
+    private fun sendNotification(){
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Order Status")
+            .setContentText("Your order has been placed!")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)){
+            notify(notificationId, builder.build())
+        }
+    }
+
+    fun onClickOrderButton(){
+        val orderButton = findViewById<Button>(R.id.order_button)
+        orderButton.setOnClickListener{
+            sendNotification()
+        }
+    }*/
+
 }
--- app/src/main/java/com/example/butterposv2/ui/order/OrderFragment.kt
+++ app/src/main/java/com/example/butterposv2/ui/order/OrderFragment.kt
@@ -1,13 +1,24 @@
 package com.example.butterposv2.ui.order
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.butterposv2.MainActivity
 import com.example.butterposv2.R
 import com.example.butterposv2.reciept_recycler_adapter
 import com.example.butterposv2.ui.theViewModel
@@ -15,12 +26,16 @@ import com.example.butterposv2.ui.theViewModel
 
 class OrderFragment : Fragment() {
 
+    private val CHANNEL_ID = "channel_id_order_notification"
+    private val notificationId = 101
+
     private lateinit var sharedViewModel: theViewModel
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: reciept_recycler_adapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         sharedViewModel = ViewModelProvider(requireActivity()).get(theViewModel::class.java)
+
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_order, container, false)
@@ -44,7 +59,47 @@ class OrderFragment : Fragment() {
         return view
     }
 
+    private fun createNotificationChannel(){
+        //If Android Oreo or above, use REQUIRED notification channel
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O ){
+            val name = "Order Status"
+            val descriptionText = "Your order has been placed!"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply{
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager = context?.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+    // For Versions < Oreo:
+    @SuppressLint("MissingPermission")
+    private fun sendNotification(){
+        val builder = view?.context?.let {
+            NotificationCompat.Builder(it, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Order Status")
+                .setContentText("Your order has been placed!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        }
+
+        if (builder != null) {
+            with(view?.context?.let { NotificationManagerCompat.from(it) }){
+                this?.notify(notificationId, builder.build())
+            }
+        }
+    }
+
+    fun orderButtonClick(){
+        createNotificationChannel()
+        val orderButton = view?.findViewById<Button>(R.id.order_button)
+        orderButton?.setOnClickListener{
+            sendNotification()
+        }
+    }
+
 }
+
 /*
 class ThirdFragment : Fragment() {
     private lateinit var sharedViewModel: SharedViewModel
